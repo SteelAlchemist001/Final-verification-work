0467892986207731516e69371d13572f382fd6d6
SteelAlchemist001 <vlad.bukhal81@gmail.com>
2022-01-31 20:00:50 +0300

    Создал программу

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿//"Контрольная работа"
+void PrintArray(string[] newarray)
+{
+    Console.Write("[");
+    for (int i = 0; i < newarray.Length; i++)
+    {
+        Console.Write($"{newarray[i]} ");
+    }
+    Console.WriteLine("]");
+}
+
+int newarraysize(string[] newarray)
+{
+    int targetarraylength = 0;
+    for (int i = 0; i < newarray.Length; i++)
+    {
+        if (newarray[i].Length <= 3)
+        {
+            targetarraylength++;
+        }
+    }
+    return targetarraylength;
+}
+
+void Fills3array(string[] newarray, string[] targetarray)
+{
+    for (int j = 0, k = 0; j < newarray.Length; j++)
+    {
+        if (newarray[j].Length <= 3)
+        {
+            targetarray[k] = newarray[j];
+            k++;
+        }
+    }
+}
+
+void Demo ()
+{
+string[] countries = {"Russia", "USA", "UE", "ЭХ", "Germany", "France", "Italy", "Sweben"};
+    Console.WriteLine(" ");
+    string[] countriesshort = new string [newarraysize(countries)];
+    Fills3array (countries, countriesshort);
+    
+    PrintArray(countriesshort);
+    
+}
+
+Demo ();
